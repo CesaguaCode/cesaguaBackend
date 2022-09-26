cd51705772ca84e5f4968fabe84245746692f624
leiton-cr <80017848+leiton-cr@users.noreply.github.com>
2022-09-26 10:13:32 -0600

    Initial commit

---
+++ cesaguaBack/src/config/server.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config';
+
+export default class Server{
+    constructor(){
+
+    }
+
+    private middlewares(){
+
+    }
+
+    private routes(){
+        
+    }
+
+    public launch(){
+
+    }
+
+}
